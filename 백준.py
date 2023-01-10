de7681cc0160692bed030271b08fdbcc1c18a7df
et009153 <et009153@gmail.com>
2023-01-10 13:55:52 +0900

    2023/01/10 13:55 학원

--- 백준.py
+++ 백준.py
@@ -1,13 +1,6 @@
-A, B = input().split()
-C = input()
-A = int(A)
-B = int(B)
-C = int(C)
-# 만약 분이랑 타이머 더한게 60분이 넘으면 
-if B + C >= 60 :
-    if A+((B+C)/60) > 24 :
-        print(int((A+((B+C)/60))%24), (B+C)%60)
-    else :
-        print(int(A+((B+C)/60)), (B+C)%60)
-else :
-    print(A, B+C)
+N = int(input())
+
+for N in range(N):
+    for N in range(N+1):
+        print("*", end="")
+    print("")
